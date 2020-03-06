5e7eb592ca82526646b14397f2f8f425fa4597f3
Belal Hamdy <belalhezzat@gmail.com>
2020-03-06 23:58:07 +0200

    fixed cloning issue

--- src/com/company/Utilities.java
+++ src/com/company/Utilities.java
@@ -1,26 +1,21 @@
 package com.company;
 
 public class Utilities {
-    public static int[][] makeCopy(int[][] array){
-        if (array != null) {
-            final int[][] copy = new int[array.length][];
+    public static int[][] makeCopy(int[][] array) {
+        if (array == null) return null;
 
-            for (int i = 0; i < array.length; i++) {
-                final int[] row = array[i];
+        final int[][] copy = new int[array.length][];
 
-                copy[i] = new int[row.length];
-                System.arraycopy(row, 0, copy[i], 0, row.length);
-            }
+        for (int i = 0; i < array.length; i++)
+            copy[i] = makeCopy(array[i]);
 
-            return copy;
-        }
-
-        return null;
+        return copy;
 
     }
-    public static int[] makeCopy(int[] array){
-        if (array != null) return array.clone();
-        return null;
+
+    public static int[] makeCopy(int[] array) {
+        if (array == null) return null;
+        return array.clone();
     }
 
 }
