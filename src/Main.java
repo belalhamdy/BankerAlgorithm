7aa3ccef442c5ba05af79dae99ccf65ec0929e8f
Belal Hamdy <belalhezzat@gmail.com>
2020-03-07 22:00:17 +0200

    Reconstruction

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,14 @@ public class Main {
             ih.fetchInputFromUser();
 
             Banker test = new Banker(ih.getAvailable(), ih.getAllocation(), ih.getMaximum());
-            System.out.println(Arrays.toString(test.getSafeSequence()));
+            int[] safeSequence = test.getSafeSequence();
+            System.out.println(Utilities.arrayToString(safeSequence,"Suggested Safe Sequence"));
             System.out.println("Enter RQ for a request, or EXIT to exit.");
             while (true) {
                 if (!ih.fetchRequest()) return;
-                System.out.println(Arrays.toString(test.request(ih.lastRequestIdx, ih.lastRequest)));
+                int[] requestSequence = test.request(ih.lastRequestIdx, ih.lastRequest);
+                if (requestSequence == null) System.out.println("No Sequence.");
+                else System.out.println(Utilities.arrayToString(requestSequence,"Suggested Safe Sequence After Request"));
             }
         } catch (Exception e) {
             e.printStackTrace();
