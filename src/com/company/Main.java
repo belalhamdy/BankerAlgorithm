4d67f914a5461aaeb35cdcf4a07791f666c916c6
Belal Hamdy <belalhezzat@gmail.com>
2020-03-07 14:11:52 +0200

    Added a second test case

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,12 +9,27 @@ public class Main {
 	int[][] alloc = {{0,1,0},{2,0,0},{3,0,2}};
     int[] avail =  {5,4,5};
 
-	int[][] need = {{7,4,3},{1,2,2},{6,0,0}};
+    int[][] max2 = {{7,5,3},{3,2,2},{9,0,2},{2,2,2},{4,3,3}};
+	int[][] alloc2 = {{0,1,0},{2,0,0},{3,0,2},{2,1,1},{0,0,2}};
+    int[] avail2 =  {3,3,2};
+
 
         try {
             Banker test = new Banker(avail,alloc,max);
             System.out.println(Arrays.toString(test.getSafeSequence()));
             System.out.println(Arrays.toString(test.request(1,new int[]{1,0,2})));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Banker test = new Banker(avail2,alloc2,max2);
+            System.out.println(Arrays.toString(test.getSafeSequence()));
+            System.out.println(Arrays.toString(test.request(1,new int[]{1,0,2})));
+            System.out.println(Arrays.toString(test.request(4,new int[]{3,3,0})));
+            System.out.println(Arrays.toString(test.request(0,new int[]{0,2,0})));
 
         } catch (Exception e) {
             e.printStackTrace();
