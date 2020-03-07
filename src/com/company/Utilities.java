6390a8675f924a9ff88e3c634577fc944f660c5a
Belal Hamdy <belalhezzat@gmail.com>
2020-03-07 13:06:23 +0200

    Added logging every step, WAITING FOR TESTER (DARDERY).

--- src/com/company/Utilities.java
+++ src/com/company/Utilities.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Utilities {
     public static int[][] makeCopy(int[][] array) {
         if (array == null) return null;
@@ -17,5 +19,21 @@ public class Utilities {
         if (array == null) return null;
         return array.clone();
     }
+
+    public static String arrayToString(int[] array, String name) {
+        if (array == null || array.length == 0 ) return "";
+        return name + ": " + Arrays.toString(array);
+    }
+    public static String arrayToString(boolean[] array, String name) {
+        if (array == null || array.length == 0 ) return "";
+        return name + ": " + Arrays.toString(array);
+    }
+    public static String arrayToString(int[][] array, String name) {
+        if (array == null || array.length == 0 ) return "";
+        StringBuilder ret = new StringBuilder("{\n");
+        for (int[] ints : array) ret.append(Arrays.toString(ints)).append("\n");
+        return name + ": " + ret + "}";
+    }
+
 
 }
